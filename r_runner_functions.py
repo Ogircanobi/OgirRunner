dc1c8de29d80e51b0d00a780782867b80224e33a
Ogircanobi <98047897+Ogircanobi@users.noreply.github.com>
2023-05-25 17:29:45 -0500

    Testing pygame commit menu

--- r_runner_functions.py
+++ r_runner_functions.py
@@ -2,8 +2,8 @@ import pygame
 
 
 # Function that displays score on screen
-def display_score(start_time,test_font,screen):
-    current_time = int(pygame.time.get_ticks()/1000) - start_time  # gets current time and saves it as current_time variable. Subtracts start time when you die and restart.
+def display_score(start_time, test_font, screen):
+    current_time = int(pygame.time.get_ticks()/1000) - start_time  # gets current time/resets start time when you die.
     score_surf = test_font.render(f'Score: {current_time}', False, (64, 64, 64))  # new surface for score
     score_rect = score_surf.get_rect(center=(400, 50))  # rectangle for score_surf
     screen.blit(score_surf, score_rect)  # Draw out on screen
